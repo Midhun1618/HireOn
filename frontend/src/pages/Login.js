0165f77f311597f2b50f8cefc5a748452a010fdb
Midhun1618 <midhumidhun342@gmail.com>
2025-06-10 21:00:18 +0530

    added Login.css

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from "react";
+import './Login.css'
 
-function Login() {
+  const Login = () => {
+    const [formData, setFormData] = useState({
+      name: "",
+      email: "",
+      confirmPassword: ""
+    });
+  
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData({ ...formData, [name]: value });
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if ("abc" !== formData.confirmPassword) {
+        alert("Passwords do not match");
+        return;
+      }
+      console.log("Form submitted", formData);
+    };
+  
   return (
-    <div>
-      
+    <div className="login-container">
+      <span>Login to</span>
+       <div className="home-logo">
+          <span className="orange-text">H</span>
+          <span className="gray-texst">ire</span>
+          <span className="orange-text">On</span>
+        </div>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="User Name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          name="Password"
+          placeholder="Confirm Password"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
     </div>
   )
 }
